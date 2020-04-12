dadbfa2421a7e7d1f6d29a90bbc76c4259dfc4c5
Mohammed Mamun <momamun@yahoo.com>
2020-04-11 20:20:48 -0400

    MoveZeroes problem

--- Week 1/Maximum-Subarray.js
+++ Week 1/Maximum-Subarray.js
@@ -12,3 +12,17 @@ function maxSubArray(nums) {
     }
     return ans
 };
+
+
+var maxProfit = function(prices) {
+    profit = 0
+    for(let i = 0; i < prices.length; i++) {
+        if(prices[i + 1] - prices[i] > 0) {
+            profit += (prices[i + 1] - prices[i])
+        }
+    }
+    return profit
+};
+
+console.log(maxProfit([1,2,3,4,5]))
+
