eda3635204d598554e3883f4b3406a2a870a9de0
Mohammed Mamun <momamun@yahoo.com>
2020-04-16 14:16:12 -0400

    Product of Array Except Self

---
+++ Week 3/Product-of-Array-Except-Self.js
@@ -0,0 +1,31 @@
+// Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+// Example:
+
+// Input:  [1,2,3,4]
+// Output: [24,12,8,6]
+// Constraint: It's guaranteed that the product of the elements of any prefix or suffix of the array (including the whole array) fits in a 32 bit integer.
+
+// Note: Please solve it without division and in O(n).
+
+
+var productExceptSelf = function(nums) {
+    const L = nums.length 
+  
+    const output = new Array(L)
+  
+    output[0] = [1]
+  
+    for(let i = 1; i < L; i++) {
+      output[i] = output[i - 1] * nums[i - 1]
+      console.log(output[i])
+    }
+  
+    let R = 1
+    for(let i = L - 1; i >= 0; i--) { 
+      output[i] = output[i] * R       
+      R = R * nums[i]                 
+    } 
+  
+    return output
+  };
